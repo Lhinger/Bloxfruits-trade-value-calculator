2c31098af14dc1af40770702c9847baf387f6e34
Lhinger <93882461+Lhinger@users.noreply.github.com>
2023-02-22 15:44:46 -0700

    Update calc.py
    made easier to read and added flair

--- calc.py
+++ calc.py
@@ -4,51 +4,48 @@ def price_of(fruit_name: str) -> int: return name_to_price[fruit_name]
 
 
 name_to_price = {
-    "blank": 0,
-    "none": 0,
-    "kilo": 7500, "Kilo": 7500,
-    "spin": 10000, "Spin": 10000,
-    "chop": 100000, "Chop": 100000,
-    "spring": 20000, "Spring": 20000,
-    "bomb": 30000, "Bomb": 30000,
-    "smoke": 200000, "Smoke": 200000,
-    "spike": 100000, "Spike": 100000,
-    "flame": 450000, "Flame": 450000,
-    "falcon": 200000, "Falcon": 200000,
-    "bird: falcon": 200000, "Bird: Falcon": 200000,
-    "ice": 800000, "Ice": 800000,
-    "sand": 750000, "Sand": 750000,
-    "dark": 1000000, "Dark": 1000000,
-    "revive": 450000, "Revive": 450000,
-    "diamond": 550000, "Diamond": 550000, "daimond": 550000, "dimond": 550000,
-    "light": 900000, "Light": 900000,
-    "love": 350000, "Love": 350000,
-    "rubber": 400000, "Rubber": 400000,
-    "barrier": 400000, "Barrier": 400000,
-    "magma": 1100000, "Magma": 1100000,
-    "quake": 1100000, "Quake": 1100000,
-    "buddha": 2300000, "Buddha": 2300000, "human: buddha": 2300000, "buddah": 2300000, "budha": 2300000,
-    "string": 1600000, "String": 1600000,
-    "phoenix": 1700000, "Phoenix": 1700000, "bird: pheonix": 1700000, "pheonix": 1700000, "phenix": 1700000,
-    "fenix": 1700000,
-    "portal": 2200000, "Portal": 2200000,
-    "rumble": 2450000, "Rumble": 2450000,
-    "paw": 1750000, "Paw": 1750000,
-    "blizzard": 2700000, "Blizzard": 2700000, "blizard": 2700000,
-    "gravity": 2600000, "Gravity": 2600000,
-    "dough": 4400000, "Dough": 4400000,
-    "shadow": 3300000, "Shadow": 3300000,
-    "venom": 4000000, "Venom": 4000000,
-    "control": 2600000, "Control": 2600000,
-    "spirit": 2500000, "Spirit": 2500000,
-    "dragon": 4200000, "Dragon": 4200000,
-    "leopard": 7800000, "Leopard": 7800000, "lepard": 7800000, "leo": 7800000
+    "blank": 0, "none": 0, "0": 0, "nothing": 0, "no": 0,
+    "kilo": 7500,
+    "spin": 10000,
+    "chop": 100000,
+    "spring": 20000,
+    "bomb": 30000,
+    "smoke": 200000,
+    "spike": 100000,
+    "flame": 450000,
+    "falcon": 200000, "bird: falcon": 200000,
+    "ice": 800000,
+    "sand": 750000,
+    "dark": 1000000,
+    "revive": 450000,
+    "diamond": 550000, "daimond": 550000, "dimond": 550000,
+    "light": 900000,
+    "love": 350000,
+    "rubber": 400000,
+    "barrier": 400000,
+    "magma": 1100000,
+    "quake": 1100000,
+    "buddha": 2300000, "human: buddha": 2300000, "buddah": 2300000, "budha": 2300000,
+    "string": 1600000,
+    "phoenix": 1700000, "bird: pheonix": 1700000, "pheonix": 1700000, "phenix": 1700000, "fenix": 1700000,
+    "portal": 2200000,
+    "rumble": 2450000,
+    "paw": 1750000,
+    "blizzard": 2700000, "blizard": 2700000,
+    "gravity": 2600000,
+    "dough": 4400000,
+    "shadow": 3300000,
+    "venom": 4000000,
+    "control": 2600000,
+    "spirit": 2500000,
+    "dragon": 4200000,
+    "leopard": 7800000, "lepard": 7800000, "leo": 7800000
 }
 
-offerfruit1 = input("Enter the first fruit you are offering: ")
-offerfruit2 = input("Enter the second fruit you are offering: ")
-offerfruit3 = input("Enter the third fruit you are offering: ")
-offerfruit4 = input("Enter the fourth fruit you are offering: ")
+offerfruit1 = input("Enter the first fruit you are offering: ").lower()
+offerfruit2 = input("Enter the second fruit you are offering: ").lower()
+offerfruit3 = input("Enter the third fruit you are offering: ").lower()
+offerfruit4 = input("Enter the fourth fruit you are offering: ").lower()
 
 print(offerfruit1, price_of(offerfruit1), "+", offerfruit2, price_of(offerfruit2), "+", offerfruit3,
       price_of(offerfruit3), "+", offerfruit4, price_of(offerfruit4), "=",
@@ -56,10 +53,10 @@ print(offerfruit1, price_of(offerfruit1), "+", offerfruit2, price_of(offerfruit2
 
 offertotal = price_of(offerfruit1) + price_of(offerfruit2) + price_of(offerfruit3) + price_of(offerfruit4)
 
-receivefruit1 = input("Enter the first fruit you are receiving: ")
-receivefruit2 = input("Enter the second fruit you are receiving: ")
-receivefruit3 = input("Enter the third fruit you are receiving: ")
-receivefruit4 = input("Enter the fourth fruit you are receiving: ")
+receivefruit1 = input("Enter the first fruit you are receiving: ").lower()
+receivefruit2 = input("Enter the second fruit you are receiving: ").lower()
+receivefruit3 = input("Enter the third fruit you are receiving: ").lower()
+receivefruit4 = input("Enter the fourth fruit you are receiving: ").lower()
 
 receivetotal = price_of(receivefruit1) + price_of(receivefruit2) + price_of(receivefruit3) + price_of(receivefruit4)
 
@@ -71,8 +68,8 @@ else:
           price_of(receivefruit1) + price_of(receivefruit2) + price_of(receivefruit3) + price_of(receivefruit4))
 
 if offertotal >= (40/100 * receivetotal) and receivetotal >= (110/100 * offertotal):
-    print("W trade :)")
+    print("\033[1;32;40m W trade :) \n")
 elif offertotal >= (40/100 * receivetotal) and (90 / 100 * offertotal) < receivetotal < (110 / 100 * offertotal):
-    print("Fair trade")
+    print("\033[1;33;40mFair trade")
 elif offertotal >= (40/100 * receivetotal) and receivetotal < (90/100 * offertotal):
-    print("L trade :(")
+    print("\033[1;31;40m L trade :( \n")
